a0f112617c107c6edba57673e3059d3e1f0d2602
Soraia G. Siquelli <114179140+soraiasiquelli@users.noreply.github.com>
2024-03-30 21:47:26 -0300

    Versão Final 0.0.1
    Essa é a primeira versão finalizada do projeto.

--- montagemdapizza.js
+++ montagemdapizza.js
@@ -1,52 +1,59 @@
 // Declaração de Variáveis
 var formopcoes = document.getElementById("opcoes");
-var formenvioinfo = document.getElementById("envioinfo")
+var formenvioinfo = document.getElementById("envioinfo");
 var conteudomontagem = document.getElementById("montagem");
 var sectionpersonalizacao = document.getElementById("sectionpersonalizacao");
 var sectionmontagem = document.getElementById("montagem");
 var escolhasdoces = document.getElementById("pizzasdoces");
+var pizza = document.getElementById("pizza");
 var escolhassalgadas = document.getElementById("pizzassalgadas");
-formenvioinfo.style.display = "none"
+var divdoce = document.getElementById("divdoce");
+var divsalgada = document.getElementById("divsalgada");
+var quebraDeLinha = document.createElement("br");
+
+formenvioinfo.style.display = "none";
+divdoce.style.display = "none";
+divsalgada.style.display = "none";
 
 function chamar() {
-    var selectSabores = document.getElementById("saborespizza");
+    var quantSabores = document.getElementById("saborespizza");
     var selectTamanho = document.getElementById("selecttamanho");
     var tamanhoSelecionado = selectTamanho.value;
     var selectTipo = document.getElementById("selecttipo");
     var tipoSelecionado = selectTipo.value;
-    var quantidadeSabores = parseInt(selectSabores.value); /*pega o número*/
-    formenvioinfo.style.display = "block"
-
+    var quantidadeSabores = parseInt(quantSabores.value); /*pega o número*/
+    formenvioinfo.style.display = "block";
 
     // Exibe os selects de pizzas doces ou salgadas a quantidade de vezes selecionada
-    for (var i = 0; i < (quantidadeSabores-1); i++) {
+    for (var i = 0; i < quantidadeSabores - 1; i++) {
         if (tipoSelecionado === "doce") {
+            pizza.style.backgroundImage = "url('images/texturamarrom.jpg')";
             var novoSelectDoce = escolhasdoces.cloneNode(true);
             novoSelectDoce.id = "escolhasdoces_" + (i + 1);
-            novoSelectDoce.style.display = "block"
+            novoSelectDoce.style.display = "block";
+            var labelDoce = novoSelectDoce.querySelector("label");
+            labelDoce.textContent = "Selecione o sabor número " + (i + 2); // Adiciona o número ao texto do label
             escolhasdoces.parentNode.appendChild(novoSelectDoce);
+            escolhasdoces.parentNode.insertBefore(quebraDeLinha, novoSelectDoce);
         } else if (tipoSelecionado === "salgada") {
             var novoSelectSalgada = escolhassalgadas.cloneNode(true);
             novoSelectSalgada.id = "escolhassalgadas_" + (i + 1);
-            novoSelectSalgada.style.display = "block"
+            novoSelectSalgada.style.display = "block";
+            var labelSalgada = novoSelectSalgada.querySelector("label"); /*vai pegar o label desse novoSelectDoce*/
+            labelSalgada.textContent = "Selecione o sabor número " + (i + 2); // Adiciona o número ao texto do label
             escolhassalgadas.parentNode.appendChild(novoSelectSalgada);
+            escolhassalgadas.parentNode.insertBefore(quebraDeLinha.cloneNode(), novoSelectSalgada);
         }
     }
 }
 
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Esconde as escolhas de pizzas doces e salgadas
     escolhasdoces.style.display = "none";
     escolhassalgadas.style.display = "none";
 
     /*Evento que vai ocorrer após clique no botão próximo que vai enviar*/
-    formopcoes.addEventListener('submit', function(event) {
+    formopcoes.addEventListener("submit", function (event) {
         event.preventDefault();
 
         // Obtém o tipo de pizza selecionado
@@ -59,8 +66,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Mostra as escolhas de pizzas doces ou salgadas, dependendo do tipo selecionado
         if (tipoSelecionado === "doce") {
+            divdoce.style.display = "block";
             escolhasdoces.style.display = "block";
         } else if (tipoSelecionado === "salgada") {
+            divdoce.style.display = "none";
+            divsalgada.style.display = "block";
             escolhassalgadas.style.display = "block";
         }
 
@@ -69,24 +79,16 @@ document.addEventListener('DOMContentLoaded', function() {
         var tamanhoSelecionado = selectTamanho.value;
         localStorage.setItem("Tamanho da Pizza", tamanhoSelecionado);
 
-       
+        var saborSelecionado = quantSabores.value;
+        localStorage.setItem("Sabor Selecionado", saborSelecionado);
 
-
+        var tipoSelecionado = selectTipo.value;
+        localStorage.setItem("Tipo Selecionado", tipoSelecionado);
     });
 
-    formenvioinfo.addEventListener('submit', function(event){
-
-
+    formenvioinfo.addEventListener("submit", function (event) {
         var textIng = document.getElementById("texting");
-        var textIngDig = textIng.value
+        var textIngDig = textIng.value;
         localStorage.setItem("Ingredientes escolhidos", textIngDig);
-
-
-
-
-
-
     });
-
-
 });
